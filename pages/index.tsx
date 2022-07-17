768eb8638518eae8a1567321526b2cf816febe6d
YooJiho <jhyoo9161@gmail.com>
2022-07-17 17:57:33 +0900

    init

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="bg-red-500">
+      <h1 className="bg-blue-500">it works!</h1>
+    </div>
+  );
+};
+
+export default Home;
