d091064f13be93f8e706924989d28450992790af
YooJiho <jhyoo9161@gmail.com>
2022-07-19 00:49:05 +0900

    Complete publishing of auth page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,7 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return (
-    <div className="bg-red-500">
-      <h1 className="bg-blue-500">it works!</h1>
-    </div>
-  );
+  return null;
 };
 
 export default Home;
