25aeeabfb5e5177bb84e7ca7621657c7918231ad
YooJiho <jhyoo9161@gmail.com>
2022-08-17 00:58:35 +0900

    Add token confirm frame

---
+++ pages/api/users/confirm.tsx
@@ -0,0 +1,15 @@
+import client from "@libs/server/client";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import { NextApiRequest, NextApiResponse } from "next";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  console.log(req.body.token);
+  res.json({
+    ok: true,
+  });
+}
+
+export default withHandler("POST", handler);
--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,6 +1,9 @@
+import twilio from "twilio";
 import client from "@libs/server/client";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
+
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
 async function handler(
   req: NextApiRequest,
@@ -14,8 +17,31 @@ async function handler(
       ok: false,
     });
   }
-
   const payload = Math.floor(100000 + Math.random() * 900000) + "";
+  const token = await client.token.create({
+    data: {
+      payload,
+      user: {
+        connectOrCreate: {
+          where: {
+            ...user,
+          },
+          create: {
+            ...user,
+            name: "Anonymous",
+          },
+        },
+      },
+    },
+  });
+  if (phone) {
+    // const message = await twilioClient.messages.create({
+    //   messagingServiceSid: process.env.TWILIO_MESSAGE_SID,
+    //   to: process.env.MY_PHONE_NUMBER!,
+    //   body: `Your login token is ${payload}`,
+    // });
+    // console.log(message);
+  }
 
   await client.user.upsert({
     where: {
