071abac377108811aa4d1bd4688bde8a467b84dd
YooJiho <jhyoo9161@gmail.com>
2022-09-12 17:37:23 +0900

    Complete upload api

---
+++ pages/api/products/index.ts
@@ -0,0 +1,35 @@
+import { withApiSession } from "@libs/server/withSession";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import { NextApiRequest, NextApiResponse } from "next";
+import client from "@libs/server/client";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    body: { name, price, description },
+    session: { user },
+  } = req;
+
+  const product = await client.product.create({
+    data: {
+      name,
+      price: +price,
+      description,
+      image: "temp_image.png",
+      user: {
+        connect: {
+          id: user?.id,
+        },
+      },
+    },
+  });
+
+  res.json({
+    ok: true,
+    product,
+  });
+}
+
+export default withApiSession(withHandler("POST", handler));
